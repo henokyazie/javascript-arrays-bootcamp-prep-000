65357740d45724dacb80f63b3d3cc0736e07eee7
brave-ping-4201 <h_yazie@yahoo.com>
2020-01-23 09:32:01 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,73 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+ 
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+ 
+var tvShows = ["game of thrones", "true detective", "the good wife", "empire"];
+ 
+var weirdGreeting = [ "he", 110, "w", 0, "r", 1, "d" ];
+ 
+var empty = [];
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+var superheroines = ["catwoman", "she-hulk", "mystique"];
+superheroines.push("wonder woman");
+var cities = ["New York", "San Francisco"]
+cities.unshift("philadelphia")
+const cats = ["Milo", "Garfield"]
+
+// we need a new variable:
+const moreCats = ["Felix", ...cats]
+
+//todo adding an element
+//define two functions, addElementToBeginningOfArray and destructivelyAddElementToBeginningOfArray.
+var teams = ["Manchester","Arsenal","Liverpool"];
+teams.push("Chelsea");
+var entrepreneurs = ["Oprah Winfrey", "Laurene Powell Jobs", "Arianna Huffington"];
+console.log(entrepreneurs[0]);
+console.log(entrepreneurs[2]+ bio);
+
+//the line below will return undefined
+entrepreneurs[9]
+var footballers = ["Messi","Ronaldo","Rooney"];
+console.log(footballers[2]);
+
+//removing an element from the beginning SHIFT 
+const days = ["Monday","Tuesday","Wednsday"]
+days.shift()
+
+//removing an element from the end of an Array
+var iceCreams = ["chocolate","vanilla","raspberry"]
+iceCreams.pop()
+
+//todo removing the end of an Array
+var news = ["CNN","BBC","Fox"]
+news.pop()
+
+object.keys([1,2,3])
+
+
+
+
+
+
+
